85b8e90638a0c0470c536877f203583238fabe8d
Divider <it-cio@mail.ru>
2021-10-04 11:09:23 +0300

    hotfix

--- prognosis.py
+++ prognosis.py
@@ -26,7 +26,7 @@ async def weather_request():
 
             covid = f'Прирост за день:\n{day}\nОбщее число заражений:\n{total}\nВ Краснодарском крае:\n{reg}'
 
-            print(covid)
+            # print(covid)
             return covid
 
 loop = asyncio.get_event_loop()
